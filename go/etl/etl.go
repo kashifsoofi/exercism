c2d835c18aa03fe83da4c03c4c7a9d20f6bbe3c6
Kashif Jamal Soofi <kashif.soofi+git@gmail.com>
2020-08-21 20:37:10 +0100

    go etl solution

--- go/etl/etl.go
+++ go/etl/etl.go
@@ -1,1 +1,19 @@
+// Package etl implements a simple library that
+// implements transform step of an Extract-Transform-Load
 package etl
+
+import (
+	"strings"
+)
+
+// Transform given Scrabble score data in an old format
+// transforms and returns data in new format
+func Transform(input map[int][]string) map[string]int {
+	output := map[string]int{}
+	for k, v := range input {
+		for _, s := range v {
+			output[strings.ToLower(s)] = k
+		}
+	}
+	return output
+}
