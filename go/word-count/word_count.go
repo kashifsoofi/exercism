ef357041dcf632e5a4840948cdb5688557193baa
Kashif Soofi <kashif.soofi+git@gmail.com>
2020-08-27 15:59:42 +0100

    go word count solution

--- go/word-count/word_count.go
+++ go/word-count/word_count.go
@@ -1,1 +1,23 @@
+// Package wordcount implements a simple library for counting words in a phrase.
 package wordcount
+
+import (
+	"strings"
+	"unicode"
+)
+
+// Frequency is frequency of words in phrase
+type Frequency map[string]int
+
+// WordCount returns a map of words and their count in a phrase.
+func WordCount(phrase string) Frequency {
+	words := strings.FieldsFunc(strings.ToLower(phrase), func(r rune) bool {
+		return !(unicode.IsLetter(r) || unicode.IsDigit(r) || r == '\'')
+	})
+
+	f := Frequency{}
+	for _, w := range words {
+		f[strings.Trim(w, "'")]++
+	}
+	return f
+}
