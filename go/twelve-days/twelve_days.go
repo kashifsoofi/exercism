fe4119b3f427f5e1957672e67b79659beecc0f55
Kashif Jamal Soofi <kashif.soofi+git@gmail.com>
2020-08-30 11:47:11 +0100

    go twelve days, removed extra logic simplified solution

--- go/twelve-days/twelve_days.go
+++ go/twelve-days/twelve_days.go
@@ -4,63 +4,30 @@
 package twelve
 
 import (
-	"fmt"
 	"strings"
 )
 
-var dayName = map[int]string{
-	1:  "first",
-	2:  "second",
-	3:  "third",
-	4:  "fourth",
-	5:  "fifth",
-	6:  "sixth",
-	7:  "seventh",
-	8:  "eighth",
-	9:  "ninth",
-	10: "tenth",
-	11: "eleventh",
-	12: "twelfth",
-}
-
-var gifts = []string{
-	"a Partridge in a Pear Tree",
-	"two Turtle Doves",
-	"three French Hens",
-	"four Calling Birds",
-	"five Gold Rings",
-	"six Geese-a-Laying",
-	"seven Swans-a-Swimming",
-	"eight Maids-a-Milking",
-	"nine Ladies Dancing",
-	"ten Lords-a-Leaping",
-	"eleven Pipers Piping",
-	"twelve Drummers Drumming",
+var verses = []string{
+	"On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.",
+	"On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
 }
 
 // Verse given a verse number return specified verse from `The Twelve Days of Christmas`
 func Verse(n int) string {
-	var giftsFoDay strings.Builder
-	for i := n - 1; i > 0; i-- {
-		giftsFoDay.WriteString(gifts[i])
-		giftsFoDay.WriteString(", ")
-	}
-	if giftsFoDay.Len() > 0 {
-		giftsFoDay.WriteString("and ")
-	}
-	giftsFoDay.WriteString(gifts[0])
-
-	return fmt.Sprintf("On the %s day of Christmas my true love gave to me: %s.", dayName[n], giftsFoDay.String())
+	return verses[n-1]
 }
 
 // Song returns complete `The Twelve Days of Christmas` song
 func Song() string {
-	var song strings.Builder
-	for n := 1; n <= 12; n++ {
-		song.WriteString(Verse(n))
-		if n < 12 {
-			song.WriteString("\n")
-		}
-	}
-	return song.String()
+	return strings.Join(verses, "\n")
 }
