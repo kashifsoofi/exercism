2d4204d45a87cfcb86105e7e8545c0a9e0659cb2
Kashif Jamal Soofi <kashif.soofi+git@gmail.com>
2020-08-30 21:46:23 +0100

    csharp matrix solution

--- csharp/matrix/Matrix.cs
+++ csharp/matrix/Matrix.cs
@@ -1,34 +1,38 @@
 ﻿using System;
+using System.Linq;
 
 public class Matrix
 {
+    private readonly int[][] _matrix;
+
     public Matrix(string input)
     {
-    }
-
-    public int Rows
-    {
-        get
+        var rows = input.Split("\n", StringSplitOptions.RemoveEmptyEntries);
+        _matrix = new int[rows.Length][];
+        for (var i = 0; i < rows.Length; i++)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            var vals = rows[i].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse).ToArray();
+            _matrix[i] = new int[vals.Length];
+            for (var j = 0; j < vals.Length; j++)
+            {
+                _matrix[i][j] = vals[j];
+            }
         }
     }
 
-    public int Cols
-    {
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
-    }
+    public int Rows => _matrix.Length;
 
-    public int[] Row(int row)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public int Cols => _matrix[0].Length;
+
+    public int[] Row(int row) => _matrix[row-1];
 
     public int[] Column(int col)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var column = new int[_matrix.Length];
+        for (var i = 0; i < _matrix.Length; i++)
+        {
+            column[i] = _matrix[i][col-1];
+        }
+        return column;
     }
 }
