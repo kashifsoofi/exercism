b8413f47272190fdad14ef4cc85c02f636415ff3
Kashif Jamal Soofi <kashif.soofi+git@gmail.com>
2020-08-30 11:33:11 +0100

    go collatz conjecture solution

--- go/collatz-conjecture/collatz_conjecture.go
+++ go/collatz-conjecture/collatz_conjecture.go
@@ -1,1 +1,22 @@
+// Package collatzconjecture implements utility routine for calculation steps required to reach 1
 package collatzconjecture
+
+import "errors"
+
+// CollatzConjecture returns steps to reach 1 if successful
+func CollatzConjecture(n int) (int, error) {
+	if n <= 0 {
+		return 0, errors.New("invalid number")
+	}
+
+	steps := 0
+	for ; n != 1; steps++ {
+		if n%2 == 0 {
+			n = n / 2
+		} else {
+			n = n*3 + 1
+		}
+	}
+
+	return steps, nil
+}
