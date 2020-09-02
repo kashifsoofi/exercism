89f59ddb4117b7cf77a3338d2213078c55aa1f13
Kashif Soofi <kashif.soofi+git@gmail.com>
2020-09-02 12:07:08 +0100

    Roman Numerals solution

--- csharp/roman-numerals/RomanNumerals.cs
+++ csharp/roman-numerals/RomanNumerals.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 public static class RomanNumeralExtension
 {
+    private static Dictionary<int, string> _valueSymbols = new Dictionary<int, string>
+        {
+            { 1, "I" },
+            { 2, "II" },
+            { 3, "III" },
+            { 4, "IV" },
+            { 5, "V" },
+            { 9, "IX" },
+            { 10, "X" },
+            { 40, "XL" },
+            { 50, "L" },
+            { 90, "XC" },
+            { 100, "C" },
+            { 400, "CD" },
+            { 500, "D" },
+            { 900, "CM" },
+            { 1000, "M" }
+        };
+
     public static string ToRoman(this int value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (value < 1 || value > 3999)
+        {
+                throw new ArgumentOutOfRangeException("Value must be between 1 and 3999.");
+        }
+
+        var valueToConvert = value;
+
+        StringBuilder romanNumerals = new StringBuilder();
+        var keys = _valueSymbols.Select(kv => kv.Key).OrderByDescending(x => x).ToList();
+        for (int i = 0; i < keys.Count; i++)
+        {
+            int key = keys[i];
+            while (valueToConvert >= key)
+            {
+                romanNumerals.Append(_valueSymbols[key]);
+                valueToConvert -= key;
+            }
+
+            if (valueToConvert == 0)
+                break;
+        }
+
+        return romanNumerals.ToString();
     }
 }
