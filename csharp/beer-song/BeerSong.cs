c7a7560259aa8ec5aaf5aa76a811c1891760685a
Kashif Soofi <kashif.soofi+git@gmail.com>
2020-09-04 18:08:39 +0100

    beer song solution

--- csharp/beer-song/BeerSong.cs
+++ csharp/beer-song/BeerSong.cs
@@ -1,9 +1,21 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 public static class BeerSong
 {
     public static string Recite(int startBottles, int takeDown)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var verses = new List<string>();
+        for (var i = startBottles; i > startBottles - takeDown; i--)
+        {
+            var verse = i switch
+            {
+                0 => "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.",
+                1 => "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.",
+                2 => "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.",
+                _ => $"{i} bottles of beer on the wall, {i} bottles of beer.\nTake one down and pass it around, {i - 1} bottles of beer on the wall."
+            };
+            verses.Add(verse);
+        }
+        return string.Join("\n\n", verses);
     }
 }
