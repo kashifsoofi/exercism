14a9e27e7d7ec218f4b5f383673e5ec9d1b2f4b3
Kashif Jamal Soofi <kashif.soofi+git@gmail.com>
2020-09-05 19:05:23 +0100

    sublist solution

--- csharp/sublist/Sublist.cs
+++ csharp/sublist/Sublist.cs
@@ -14,6 +14,56 @@ public static class Sublist
     public static SublistType Classify<T>(List<T> list1, List<T> list2)
         where T : IComparable
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int IndexOf(List<T> list, T item, int startIndex = 0)
+        {
+            for (var i = startIndex; i < list.Count; i++)
+            {
+                return i;
+            }
+            return -1;
+        }
+
+        bool IsSublist(List<T> list1, List<T> list2)
+        {
+            if (list1.Count == 0)
+            {
+                return true;
+            }
+
+            var i = IndexOf(list2, list1[0]);
+            if (i == -1 || list1.Count > list2.Count - i)
+            {
+                return false;
+            }
+
+            for (var j = 0; j < list1.Count; j++)
+            {
+                if (list1[j].CompareTo(list2[i + j]) != 0)
+                {
+                    i = IndexOf(list2, list1[0], i + 1);
+                    if (i == -1 || list1.Count > list2.Count - i)
+                    {
+                        return false;
+                    }
+                    j = -1;
+                }
+            }
+            return true;
+        }
+
+        if (list1.Count == list2.Count && IsSublist(list1, list2))
+        {
+            return SublistType.Equal;
+        }
+        else if (list1.Count < list2.Count && IsSublist(list1, list2))
+        {
+            return SublistType.Sublist;
+        }
+        else if (list1.Count > list2.Count && IsSublist(list2, list1))
+        {
+            return SublistType.Superlist;
+        }
+
+        return SublistType.Unequal;
     }
 }
