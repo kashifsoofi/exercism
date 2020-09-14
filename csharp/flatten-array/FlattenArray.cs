2ff03f27fb4c75f9d41fdb4c2e0893b644dece95
Kashif Soofi <kashif.soofi+git@gmail.com>
2020-09-14 11:57:33 +0100

    csharp solution
    Signed-off-by: Kashif Soofi <kashif.soofi+git@gmail.com>

--- csharp/flatten-array/FlattenArray.cs
+++ csharp/flatten-array/FlattenArray.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 
 public static class FlattenArray
 {
     public static IEnumerable Flatten(IEnumerable input)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        void Flatten(IEnumerable input, List<object> output)
+        {
+            foreach (var item in input)
+            {
+                var enumerable = item as IEnumerable;
+                if (enumerable != null)
+                {
+                    Flatten(enumerable, output);
+                }
+                else if (item != null)
+                {
+                    output.Add(item);
+                }
+            }
+        }
+
+        var flattened = new List<object>();
+        Flatten(input, flattened);
+        return flattened;
     }
 }
