abb565ddc20faab05ae2c7a499d3fbf91e922ca0
Kashif Jamal Soofi <kashif.soofi+git@gmail.com>
2020-09-17 21:34:44 +0100

    c# solution

--- csharp/binary-search/BinarySearch.cs
+++ csharp/binary-search/BinarySearch.cs
@@ -4,6 +4,24 @@ public static class BinarySearch
 {
     public static int Find(int[] input, int value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int left = 0, right = input.Length - 1;
+        while (left <= right)
+        {
+            var mid = left + (right - left) / 2;
+            if (input[mid] == value)
+            {
+                return mid;
+            }
+
+            if (input[mid] < value)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+        return -1;
     }
 }
