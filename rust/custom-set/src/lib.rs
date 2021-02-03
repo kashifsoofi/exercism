5e9c1e18dc8b4517aded9d7bc0dd11214579e8fc
Kashif Soofi <kashif.soofi+git@gmail.com>
2021-02-03 16:36:04 +0000

    rust custom set exercise solution
    Signed-off-by: Kashif Soofi <kashif.soofi+git@gmail.com>

--- rust/custom-set/src/lib.rs
+++ rust/custom-set/src/lib.rs
@@ -1,48 +1,55 @@
-use std::marker::PhantomData;
-
-#[derive(Debug, PartialEq)]
+#[derive(Debug)]
 pub struct CustomSet<T> {
-    // This field is here to make the template compile and not to
-    // complain about unused type parameter 'T'. Once you start
-    // solving the exercise, delete this field and the 'std::marker::PhantomData'
-    // import.
-    phantom: PhantomData<T>,
+    items: Vec<T>,
 }
 
-impl<T> CustomSet<T> {
-    pub fn new(_input: &[T]) -> Self {
-        unimplemented!();
+impl<T: Clone + Ord + PartialEq> PartialEq for CustomSet<T>  {
+    fn eq(&self, other: &CustomSet<T>) -> bool {
+        self.is_subset(other) && other.is_subset(self)
+    }
+}
+
+impl<T: Clone + Ord + PartialEq> CustomSet<T> {
+    pub fn new(input: &[T]) -> Self {
+        CustomSet { items: input.to_vec() }
     }
 
-    pub fn contains(&self, _element: &T) -> bool {
-        unimplemented!();
+    pub fn contains(&self, element: &T) -> bool {
+        self.items.contains(element)
     }
 
-    pub fn add(&mut self, _element: T) {
-        unimplemented!();
+    pub fn add(&mut self, element: T) {
+        if !self.contains(&element) {
+            self.items.push(element);
+        }
     }
 
-    pub fn is_subset(&self, _other: &Self) -> bool {
-        unimplemented!();
+    pub fn is_subset(&self, other: &Self) -> bool {
+        self.items.iter().all(|i| other.contains(i))
     }
 
     pub fn is_empty(&self) -> bool {
-        unimplemented!();
+        self.items.is_empty()
     }
 
-    pub fn is_disjoint(&self, _other: &Self) -> bool {
-        unimplemented!();
+    pub fn is_disjoint(&self, other: &Self) -> bool {
+        self.items.iter().all(|i| !other.contains(i))
     }
 
-    pub fn intersection(&self, _other: &Self) -> Self {
-        unimplemented!();
+    pub fn intersection(&self, other: &Self) -> Self {
+        let intersection: Vec<T> = self.items.iter().filter(|&i| other.contains(i)).cloned().collect();
+        Self::new(&intersection)
     }
 
-    pub fn difference(&self, _other: &Self) -> Self {
-        unimplemented!();
+    pub fn difference(&self, other: &Self) -> Self {
+        let difference: Vec<T> = self.items.iter().filter(|i| !other.contains(i)).cloned().collect();
+        Self::new(&difference)
     }
 
-    pub fn union(&self, _other: &Self) -> Self {
-        unimplemented!();
+    pub fn union(&self, other: &Self) -> Self {
+        let mut union : Vec<T> = self.items.iter().cloned().collect();
+        union.extend(other.items.iter().filter(|i| !self.items.contains(i)).cloned());
+
+        Self::new(&union)
     }
 }
