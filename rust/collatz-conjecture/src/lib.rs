a17cb73acdb6861b28a876d210635219de159952
Kashif Soofi <kashif.soofi+git@gmail.com>
2021-02-15 15:42:57 +0000

    Collatz Conjecture rust solution
    Signed-off-by: Kashif Soofi <kashif.soofi+git@gmail.com>

--- rust/collatz-conjecture/src/lib.rs
+++ rust/collatz-conjecture/src/lib.rs
@@ -1,6 +1,18 @@
 pub fn collatz(n: u64) -> Option<u64> {
-    unimplemented!(
-        "return Some(x) where x is the number of steps required to reach 1 starting with {}",
-        n,
-    )
+    if n <= 0 {
+        return None;
+    }
+
+    let mut num = n;
+    let mut steps = 0;
+    while num != 1 {
+        if num % 2 == 0 {
+            num = num / 2;
+        } else {
+            num = num * 3 + 1;
+        }
+        steps += 1;
+    }
+
+    Some(steps)
 }
