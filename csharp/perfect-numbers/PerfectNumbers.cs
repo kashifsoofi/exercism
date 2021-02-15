ee33acd953490bd83d73d910cb153971609f712a
Kashif Soofi <kashif.soofi+git@gmail.com>
2021-02-15 14:33:36 +0000

    Perfect Numbers C# solution
    Signed-off-by: Kashif Soofi <kashif.soofi+git@gmail.com>

--- csharp/perfect-numbers/PerfectNumbers.cs
+++ csharp/perfect-numbers/PerfectNumbers.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public enum Classification
 {
@@ -11,6 +13,24 @@ public static class PerfectNumbers
 {
     public static Classification Classify(int number)
     {
-        throw new NotImplementedException();
+        if (number < 1)
+        {
+            throw new ArgumentOutOfRangeException(nameof(number));
+        }
+
+        var  aliquotSum = 0;
+        for (var i = 1; i <= number / 2; i++)
+        {
+            if (number % i == 0)
+            {
+               aliquotSum += i;
+            }
+        }
+
+        return aliquotSum == number
+            ? Classification.Perfect
+            : aliquotSum > number
+                ? Classification.Abundant
+                : Classification.Deficient;
     }
 }
