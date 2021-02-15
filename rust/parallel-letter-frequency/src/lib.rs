3ba07a4665b2cfc91b8636d80ae27ff5a401b25e
Kashif Soofi <kashif.soofi+git@gmail.com>
2021-02-15 13:25:03 +0000

    Rust solution
    Signed-off-by: Kashif Soofi <kashif.soofi+git@gmail.com>

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -1,12 +1,40 @@
+use std::thread;
 use std::collections::HashMap;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    unimplemented!(
-        "Count the frequency of letters in the given input '{:?}'. Ensure that you are using {} to process the input.",
-        input,
-        match worker_count {
-            1 => "1 worker".to_string(),
-            _ => format!("{} workers", worker_count),
+    let mut handles = vec![];
+    let chunk_size = input.len() / worker_count + 1;
+    input
+        .chunks(chunk_size)
+        .for_each(|chunk| {
+            let data: Vec<String> = chunk.iter().map(|s| s.to_lowercase()).collect();
+            let handle = thread::spawn(move || {
+                let mut counts : HashMap<char, usize> = HashMap::new();
+
+                for line in data {
+                    line
+                        .chars()
+                        .filter(|c| c.is_alphabetic())
+                        .for_each(|ch| {
+                            let count = counts.entry(ch).or_insert(0);
+                            *count += 1;
+                        });
+                }
+
+                counts
+            });
+
+            handles.push(handle);
+        });
+
+
+    let mut char_counts : HashMap<char, usize> = HashMap::new();
+    for handle in handles {
+        for (ch, c) in handle.join().unwrap() {
+            let count = char_counts.entry(ch).or_insert(0);
+            *count += c;
         }
-    );
+    }
+
+    char_counts
 }
